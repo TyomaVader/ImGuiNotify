575c750cf848ef61847069e0332d7417609f2b1c
TyomaVader <tyomastarwars2017@gmail.com>
2024-09-12 12:53:21 +0300

    update imgui in unix example

--- unixExample/main.cpp
+++ unixExample/main.cpp
@@ -1,4 +1,5 @@
 // Dear ImGui: standalone example application for Glfw + Vulkan
+
 // Learn about Dear ImGui:
 // - FAQ                  https://dearimgui.com/faq
 // - Getting Started      https://dearimgui.com/getting-started
@@ -608,41 +609,6 @@ int main(int, char**)
     */
 
 
-
-
-
-
-    // // Upload Fonts
-    // {
-    //     // Use any command queue
-    //     VkCommandPool command_pool = wd->Frames[wd->FrameIndex].CommandPool;
-    //     VkCommandBuffer command_buffer = wd->Frames[wd->FrameIndex].CommandBuffer;
-
-    //     err = vkResetCommandPool(g_Device, command_pool, 0);
-    //     check_vk_result(err);
-    //     VkCommandBufferBeginInfo begin_info = {};
-    //     begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-    //     begin_info.flags |= VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
-    //     err = vkBeginCommandBuffer(command_buffer, &begin_info);
-    //     check_vk_result(err);
-
-    //     ImGui_ImplVulkan_CreateFontsTexture(command_buffer);
-
-    //     VkSubmitInfo end_info = {};
-    //     end_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
-    //     end_info.commandBufferCount = 1;
-    //     end_info.pCommandBuffers = &command_buffer;
-    //     err = vkEndCommandBuffer(command_buffer);
-    //     check_vk_result(err);
-    //     err = vkQueueSubmit(g_Queue, 1, &end_info, VK_NULL_HANDLE);
-    //     check_vk_result(err);
-
-    //     err = vkDeviceWaitIdle(g_Device);
-    //     check_vk_result(err);
-    //     ImGui_ImplVulkan_DestroyFontUploadObjects();
-    // }
-
-
     // Main loop
     while (!glfwWindowShouldClose(window))
     {
@@ -662,6 +628,11 @@ int main(int, char**)
             ImGui_ImplVulkanH_CreateOrResizeWindow(g_Instance, g_PhysicalDevice, g_Device, &g_MainWindowData, g_QueueFamily, g_Allocator, fb_width, fb_height, g_MinImageCount);
             g_MainWindowData.FrameIndex = 0;
             g_SwapChainRebuild = false;
+        }
+        if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0)
+        {
+            ImGui_ImplGlfw_Sleep(10);
+            continue;
         }
 
         // Start the Dear ImGui frame
